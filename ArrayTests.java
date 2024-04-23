cd94e02ddcdc5b5320b5546a9402b00931771630
Aidan Rikic <aidanrikic1020@gmail.com>
2024-04-23 10:16:04 -0700

    corrected reverseInPlace and reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,25 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+  @Test 
+  public void testReverseInPlace2(){
+    int[] exArray = { 1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(exArray);
+
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, exArray);
+  }
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+  public void testReversed2() {
+    int[] exArray = {1, 2, 3, 4, 5};
+  
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, ArrayExamples.reversed(exArray));
   }
 }
